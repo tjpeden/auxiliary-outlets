b08d1a158d5b2cedcc5786e9a359bfbf08ea705d
TJ Peden <tj.peden@tjcoding.com>
2017-09-29 13:23:52 -0500

    Seems I can't spell

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout',
   template: `
-    <h1>Auxilary Outlets Test</h1>
+    <h1>auxiliary Outlets Test</h1>
     <a href="#" [routerLink]="['/admin']">Admin</a>
     <a href="#" [routerLink]="['/clients']">Clients</a>
     <router-outlet name="aux"></router-outlet>
